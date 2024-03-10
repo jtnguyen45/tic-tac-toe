f8077877d5e3774596d4999d3f66a6c86c489cc8
jtnguyen45 <jtnguyen45@gmail.com>
2024-03-10 06:44:51 -0700

    Add renderControls(), handlePlacement() functions

--- script.js
+++ script.js
@@ -13,16 +13,18 @@ let turn;
 /*----- cached elements  -----*/
 const messageEl = document.querySelector("h1");
 const resetBtn = document.querySelector("#resetBtn");
+const boardEls = Array.from(document.querySelectorAll("#board > button"));
 
 /*----- event listeners -----*/
-
+resetBtn.addEventListener("click", init);
+document.getElementById("board").addEventListener("click", handlePlacement);
 
 /*----- functions -----*/
 init();
 
 function init() {
     board = [
-        [1, null, null],
+        [null, null, null],
         [null, null, null],
         [null, null, null]
     ];
@@ -52,14 +54,44 @@ function renderBoard() {
 
 function renderMessage() {
     if (winner === "T") {
-        messageEl.innerHTML = "Tie game!";
+        messageEl.innerHTML = "TIE GAME!";
+        disableBoard();
     } else if (winner) {
-        messageEl.innerHTML = `${SYMBOL_LOOKUP[winner]} is the winner!`;
+        messageEl.innerHTML = `${SYMBOL_LOOKUP[winner]} IS THE WINNER!`;
+        disableBoard();
     } else {
-        messageEl.innerHTML = `${SYMBOL_LOOKUP[turn]}'s turn`
+        messageEl.innerHTML = `${SYMBOL_LOOKUP[turn]}'S TURN`
     }
 }
 
+function disableBoard() {
+    document.querySelectorAll("#board > button").disabled = true;
+}
+
 function renderControls() {
-    //
+    if (winner) resetBtn.innerHTML = "PLAY AGAIN";
+}
+
+function handlePlacement(evt) {
+    const button = evt.target;
+    const cellId = button.id;
+    
+    const rowIndex = parseInt(cellId.charAt(3));
+    const colIndex = parseInt(cellId.charAt(1));
+
+    if (board[rowIndex][colIndex] !== null || winner) return;
+    board[rowIndex][colIndex] = turn;
+
+    //winner = checkWinner(rowIndex, colIndex);
+
+    turn *= -1;
+    render();
+}
+
+function checkWinner(rowIndex, colIndex) {
+    //TODO: checks horizontal
+    //TODO: checks vertical
+    //TODO: checks diagonal pos slope
+    //TODO: checks diagonal neg slope
+    //TODO: checks tie
 }
